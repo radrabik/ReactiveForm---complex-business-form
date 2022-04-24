606a5f66086b803da87cc5b7209be9172f3eb175
radrabik <radoslav.drabik@gmail.com>
2022-04-24 17:39:53 +0200

    2 files modified

--- src/app/model/course.ts
+++ src/app/model/course.ts
@@ -1,9 +1,13 @@
 export interface Course {
-  id:number;
-  description:string;
+  id: number;
+  description: string;
   iconUrl: string;
   courseListIcon: string;
   longDescription: string;
-  category:string;
-  lessonsCount:number;
+  category: string;
+  lessonsCount: number;
+}
+export interface CourseCategory {
+  code: string;
+  description: string;
 }
--- src/app/services/courses.service.ts
+++ src/app/services/courses.service.ts
@@ -1,62 +1,62 @@
-import {Injectable} from "@angular/core";
-import {HttpClient, HttpParams} from "@angular/common/http";
-import {Observable} from "rxjs";
-import {Course} from "../model/course";
-import {map} from "rxjs/operators";
-import {Lesson} from "../model/lesson";
+import { Injectable } from '@angular/core';
+import { HttpClient, HttpParams } from '@angular/common/http';
+import { Observable } from 'rxjs';
+import { of } from 'rxjs';
+import { Course, CourseCategory } from '../model/course';
+import { map } from 'rxjs/operators';
+import { Lesson } from '../model/lesson';
 
 const localUrl = 'assets/categories.json';
 
+const listCategories = [
+  { code: 'BEGINNER', description: 'Beginner' },
+  { code: 'INTERMEDIATE', description: 'Intermediate' },
+  { code: 'ADVANCED', description: 'Advanced' },
+];
+
 @Injectable()
 export class CoursesService {
+  constructor(private http: HttpClient) {}
 
-    constructor(private http:HttpClient) {
+  findCourseById(courseId: number): Observable<Course> {
+    return this.http.get<Course>(`/api/courses/${courseId}`);
+  }
 
-    }
+  findCourseCategories(): Observable<CourseCategory[]> {
+    return of(listCategories);
+  }
 
-    findCourseById(courseId: number): Observable<Course> {
-        return this.http.get<Course>(`/api/courses/${courseId}`);
-    }
+  findAllCourses(): Observable<Course[]> {
+    return this.http.get('/api/courses').pipe(map((res) => res['payload']));
+  }
 
-    findCourseCategories() {
-      return this.http.get(localUrl)
-        .pipe(
-          map(res => res["categories"])
-        );
-    }
+  findAllCourseLessons(courseId: number): Observable<Lesson[]> {
+    return this.http
+      .get('/api/lessons', {
+        params: new HttpParams()
+          .set('courseId', courseId.toString())
+          .set('pageNumber', '0')
+          .set('pageSize', '1000'),
+      })
+      .pipe(map((res) => res['payload']));
+  }
 
-    findAllCourses(): Observable<Course[]> {
-        return this.http.get('/api/courses')
-            .pipe(
-                map(res => res['payload'])
-            );
-    }
-
-    findAllCourseLessons(courseId:number): Observable<Lesson[]> {
-        return this.http.get('/api/lessons', {
-            params: new HttpParams()
-                .set('courseId', courseId.toString())
-                .set('pageNumber', "0")
-                .set('pageSize', "1000")
-        }).pipe(
-            map(res =>  res["payload"])
-        );
-    }
-
-    findLessons(
-        courseId:number, filter = '', sortOrder = 'asc',
-        pageNumber = 0, pageSize = 3):  Observable<Lesson[]> {
-
-        return this.http.get('/api/lessons', {
-            params: new HttpParams()
-                .set('courseId', courseId.toString())
-                .set('filter', filter)
-                .set('sortOrder', sortOrder)
-                .set('pageNumber', pageNumber.toString())
-                .set('pageSize', pageSize.toString())
-        }).pipe(
-            map(res =>  res["payload"])
-        );
-    }
-
+  findLessons(
+    courseId: number,
+    filter = '',
+    sortOrder = 'asc',
+    pageNumber = 0,
+    pageSize = 3
+  ): Observable<Lesson[]> {
+    return this.http
+      .get('/api/lessons', {
+        params: new HttpParams()
+          .set('courseId', courseId.toString())
+          .set('filter', filter)
+          .set('sortOrder', sortOrder)
+          .set('pageNumber', pageNumber.toString())
+          .set('pageSize', pageSize.toString()),
+      })
+      .pipe(map((res) => res['payload']));
+  }
 }
