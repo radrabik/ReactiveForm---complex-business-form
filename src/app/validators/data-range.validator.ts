81b9bd81f3d6c74ec4ccb08ea6db930fa27f4d89
radrabik <radoslav.drabik@gmail.com>
2022-04-24 23:39:05 +0200

    2 files modified

--- src/app/validators/data-range.validator.ts
+++ src/app/validators/data-range.validator.ts
@@ -3,11 +3,13 @@ import {FormGroup, ValidatorFn, Validators} from '@angular/forms';
 export function createPromoRangeValidator(): ValidatorFn {
   return (form: FormGroup): Validators | null => {
 
-    const start = form.get("promoStartAt").value;
-    const end = form.get("promoEndAt").value;
+    const start:Date = form.get("fromDate").value;
+    const end:Date = form.get("toDate").value;
 
     if (start && end ){
-      const isRangeValid = (end?.getTime() - start?.getDate() > 0)
+
+      const isRangeValid = (end.getTime() - start.getTime() > 0)
+
       return isRangeValid ? null : {promoPeriod: true}
     }
   
